1752a9b4304bca4349d85fcd9b7e22f89677b004
Rupesh Mishra <rrupesh.mishra@gmail.com>
2021-11-11 17:26:08 +0530

    test code added

---
+++ bgcolor.js
@@ -0,0 +1,10 @@
+let body = document.querySelector('body');
+        document.querySelector('#red').onclick = function() {
+            body.style.backgroundColor = 'red';
+        }
+        document.querySelector('#green').onclick = function() {
+            body.style.backgroundColor = 'green';
+        }
+        document.querySelector('#blue').onclick = function() {
+            body.style.backgroundColor = 'blue';
+        }
